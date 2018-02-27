38b2df57055133f4ec233d6d67dfd63bbb958a6a
Justin Pugliese <jmpuglie@us.ibm.com>
2018-02-26 19:43:05 -0500

    server outline

--- server.py
+++ server.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# start IDS class in 'listen' mode
+    ## wait for connection
+
+    ## read byte by byte and identifier signature
+
+        ## if sig, drop message & log envent
+
+        ## else, pass message to server
+
+# obtain message
+    ## process cmd through IDS
+
+        ### `ls`
+            #### return all file names
+
+        ### `get`
+            #### load file and send
+
+        ### `put`
+            #### save file
+
+        ### send ACK
+
+
 if __name__ == '__main__':
     sys.exit(main())
