bc5e4fea7eb2e65866e180f4e1d24bc1c0227ae6
Justin Pugliese <jmpuglie@us.ibm.com>
2018-02-26 19:38:51 -0500

    client outline

---
+++ client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+
+# start client + listen for cmd
+
+# process cmd
+    ## do `put`
+        ### upload file
+        ### send
+
+    ## do `get`
+        ### send cmd
+        ### save response as file
+
+    ## do `ls`
+        ### send cmd
+        ### output response
+
+    ## do `exit`s
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
