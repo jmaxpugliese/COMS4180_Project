f59b0e8f64672d5bf295743d3e07fe1720d24f6e
Justin Pugliese <jmpuglie@us.ibm.com>
2018-03-03 10:22:35 -0500

    started client setup

--- client.py
+++ client.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
+import socket
 
-# process cli
+def main():
+    # process cli
+    args = get_runtime_args()
 
-# start client + listen for cmd
+    # print supported cmd's to user
+    # print_help()
+
+    # listen for cmd
+    cmd = listen()
 
 # process cmd
     ## do `put`
@@ -21,7 +29,33 @@
 
     ## do `exit`s
 
+def listen():
+    cmd = input('cmd: ')
+    return cmd
 
+# Retrieve and validate runtime arguments before starting the application
+def get_runtime_args():
+    try:
+        # index 1: server ip address
+        # index 2: server port number to connect
+        if len(sys.argv) == 3:
+            # validate ip address format
+            if sys.argv[1] != 'localhost':
+                socket.inet_aton(sys.argv[1])
+            # return args as tuple
+            return (sys.argv[1], int(sys.argv[2]))
+        else:
+            exit_with_msg('Please specify Client input with format:\n`server-ip-address`\n`server-connection-port`\n')
+
+    except OSError:
+        exit_with_msg('Invalid IP Address. Please try again')
+    except ValueError:
+        exit_with_msg('Port must be an integer. Please try again.')
+
+# Print message then exit
+def exit_with_msg(m):
+    print ('\n\nProgram Exiting; ' + m)
+    exit(0)
 
 if __name__ == '__main__':
     sys.exit(main())
