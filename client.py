2103b63e1cae1ea26ac3f32ed5b8c2b2ff631811
Justin Pugliese <jmpuglie@us.ibm.com>
2018-03-03 12:50:41 -0500

    stubbed client connection fns

--- client.py
+++ client.py
@@ -8,33 +8,20 @@ def main():
     # process cli
     args = get_runtime_args()
 
+    # open connection to server
+    establish_connection(args[0], args[1])
+
     # print supported cmd's to user
     print_supported_commands('Welcome to our simple FTP client!')
 
     # prompt user for input
     prompt()
 
-    # process cmd
-    if input_tuple[0] == 'put':
-        # upload file
-        # send
-        print('put')
-
-    elif input_tuple[0] == 'get':
-        # send cmd
-        # save response as file
-        print('get')
-
-    elif input_tuple[0] == 'ls':
-        # send cmd
-        # output response
-        print('exit')
-
-    elif input_tuple[0] == 'exit':
-        print('exit')
-
-    else:
-        print_supported_commands('Unfortunately that command is not supported.')
+def establish_connection(ip_addr, socket_no):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(10)
+    s.connect((ip_addr, socket_no))
+    return s
 
 # prompt user for input
 def prompt():
@@ -63,7 +50,7 @@ def process_input(input_tuple):
     elif input_tuple[0] == 'ls':
         # send cmd
         # output response
-        print('exit')
+        print('ls')
 
     elif input_tuple[0] == 'exit':
         exit_with_msg('Thank you!')
